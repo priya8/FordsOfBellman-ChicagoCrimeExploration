51cf67fd9ff7067aa2c0c2909b86e5c78190f741
Shemal Somil Lalaji <shla8652@colorado.edu>
2019-04-03 18:59:44 -0600

    Add files via upload

---
+++ assets/js/google-map-api.js
@@ -0,0 +1,57 @@
+var map;
+
+//renders the google map
+function initMap() {
+    map = new google.maps.Map(
+        document.getElementById("map"),
+        {
+            center: {lat: 41.881832, lng: -87.623177},
+            zoom: 11
+        }
+    );
+}
+
+// initialize array of markers that will be used to clear markers from the map
+var crimeMarkers = [];
+//function that adds marker to the map where the crime was commited, takes longitude, latitude
+function addMarker(lat,lng){
+    //this will hold the path to the icon image
+    var markerIcon = null;
+
+    marker = new google.maps.Marker({
+            position: new google.maps.LatLng(lat,lng),
+            map: map,
+            icon: markerIcon
+        }
+    );
+    crimeMarkers.push(marker);
+    //sets the zoom to the location that the map is
+    map.setZoom(18);
+    map.panTo(marker.position);
+}
+
+// Removes the markers from the map,
+function clearOverlays() {
+  for (var i = 0; i < crimeMarkers.length; i++ ) {
+    crimeMarkers[i].setMap(null);
+  }
+  crimeMarkers.length = 0;
+
+}
+
+// Add info windows to each marker for crime info
+function addCrimeInfo(number, marker, date, address, description, crimeType){
+    //adding info
+    var crimeInfo = "<p> #" + number + "</p><p> Description: " + crimeType + " " + description + "</p> <p> Block: " + address + "</p><p> Date: " + date + "</p>";
+    var infowindow = new google.maps.InfoWindow({
+        content: crimeInfo
+    });
+    //added mouseover to see the info window
+    marker.addListener("mouseover", function(){
+        infowindow.open(map, marker);
+    });
+    marker.addListener("mouseout", function() {
+    infowindow.close(map, marker);
+});
+}
+
